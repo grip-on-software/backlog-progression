3857028633a9f913a7c03e7dc5cbd1b25c2bca96
Cas Dekkers <cas.dekkers@ictu.nl>
2020-04-28 15:17:35 +0200

    Remove create-react-app bloat; initialize with redux-toolkit and bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
+import rootReducer from './slices'
+
+import './index.scss';
+
+const store = configureStore({ reducer: rootReducer });
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  </Provider>,
+  document.getElementById('root'));
